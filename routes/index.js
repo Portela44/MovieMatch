3e4b77b8b23b174995034ceb043dacc5c4bca0f4
Pau Mesonero <paumesonero@Paus-Mac-mini.local>
2022-06-29 23:45:15 +0200

    only admin can see buttons

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,20 @@ const metafilm = require("metafilm");
 // @route   GET /
 // @access  Public
 router.get("/", (req, res, next) => {
-  res.render("index")
+  const user = req.session.currentUser
+  res.render("index", { user })
 })
 
 // @desc    App search page (only meant to test apis)
 // @route   GET /
 // @access  Public
 router.get('/movie-search', async (req, res, next) => {
-  const {movie} = req.query;
+  const { movie } = req.query;
   try {
-    const IMDbID = await IMDb(`${movie}`, function(err, res, inf) {
-      return(res);
+    const IMDbID = await IMDb(`${movie}`, function (err, res, inf) {
+      return (res);
     });
-    const movieInfo = await metafilm.id({imdb_id: `${IMDbID}`});
+    const movieInfo = await metafilm.id({ imdb_id: `${IMDbID}` });
     // res.render("searchResults", movieInfo);
     await res.json(movieInfo)
   } catch (error) {
