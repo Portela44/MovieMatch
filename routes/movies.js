ce4aef73257c4e5e912e01afb0f7debbdefbd7e2
Guillem <guillem.mart7@gmail.com>
2022-06-29 20:57:22 +0200

    admin tools wip

--- routes/movies.js
+++ routes/movies.js
@@ -14,6 +14,34 @@ router.get('/', async (req, res, next) => {
     } catch (error) {
         next(error)
     }
-})
+});
+
+// @desc    Displays admin menu to create a movie.
+// @route   GET /movies/create
+// @access  Admin
+
+router.get('/create', async (req, res, next) => {
+    try {
+        res.render('movies/new-movie')
+    } catch (error) {
+        next(error)
+    }
+});
+
+// @desc    Posts a new-movie information to the db.
+// @route   POST /movies/create
+// @access  Admin
+
+router.get('/create', async (req, res, next) => {
+    // const {Things go in here} = req.body
+    try {
+        await Movie.create();
+        res.redirect();
+    } catch (error) {
+        next(error)
+    }
+});
+
+
 
 module.exports = router;
